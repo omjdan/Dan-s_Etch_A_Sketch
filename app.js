cbbacf6e3079d02dd6c803c58a427de0b9b9c065
omjdanom <omjdanieldev@gmail.com>
2022-01-02 11:10:59 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const container = document.querySelector('.container');
+const buttons = document.querySelectorAll('button')
+let classPicked;
+let randomBtn = document.querySelector('.random')
+
+
+
+
+//creates divs
+let grid;
+for (let i = 0; i < 4096; i++){
+    grid =  document.createElement('div')
+grid.className = "reset"
+container.appendChild(grid)
+}
+
+//class picker buttons
+
+ document.querySelectorAll('.button').forEach(
+    btn => {
+        btn.addEventListener('click', e => {
+
+         classPicked = btn.innerText;
+
+        })
+    })
+
+
+// function changes class on hover
+
+document.querySelectorAll('.reset').forEach(itm => {
+   itm.addEventListener('mouseover', e => {
+itm.classList.remove('reset')
+    itm.classList.add(classPicked)
+})
+})
+
+
+// reset button 
+document.getElementById('res').addEventListener('click', e =>{
+container.querySelectorAll('div').forEach(itm =>{
+    itm.classList.remove('green','yellow','blue','white')
+itm.classList.add('reset')
+    
+})
+})
+
+
+// Random color  picker and hover even Listener
+let ranColor = () => document.querySelectorAll('.reset').forEach(item =>{
+item.addEventListener('mouseover', e =>{let randomClass = () =>{
+    let newCls;
+    let randomCls = Math.floor(Math.random() * 4)
+    if (randomCls === 0) newCls = 'green';
+    if (randomCls === 1) newCls = 'yellow';
+    if (randomCls ===2)  newCls  = 'blue';
+    if (randomCls === 3) newCls = 'white';
+    return newCls;
+}
+
+ 
+item.classList.remove('green','yellow','blue','white')
+item.classList.add(randomClass())
+
+
+
+})
+})
+
+randomBtn.addEventListener('click', ranColor())
+
+
